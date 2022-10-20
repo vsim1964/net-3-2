670af5a2d204347b836ac31774a5491087085f25
vsim1964 <vsim1964@gmail.com>
2022-10-20 12:49:14 +0300

    Добавил обрезание заголовка

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import Listing from './Components/Listing';
 import items from './Components/Data/items';
 
 for (let i = 0; i < items.length; i++) {
+  if (items[i].title.length > 50) {
+    items[i].title = items[i].title.substring(0, 50) + '...';
+  }
+}
+
+for (let i = 0; i < items.length; i++) {
   if (items[i].currency_code === 'USD') {
     items[i].currency_code = '$';
   } else if (items[i].currency_code === 'EUR') {
